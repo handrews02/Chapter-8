b879885fd90f96a6dbb7c62e4990178e6acd4b07
Hannah Andrews <handrews02@users.noreply.github.com>
2018-10-04 09:50:41 -0500

    updated text

--- Chapter 8/src/gameZone/Quiz.java
+++ Chapter 8/src/gameZone/Quiz.java
@@ -4,7 +4,7 @@ public class Quiz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String [] arrayAnswers = {2, Bella, Zayn};
+		String [] arrayAnswers = {two, Bella, Zayn};
 		
 		System.out.println("What is 1 + 1?");
 		System.out.println("Who is the main character in Twilight?");
